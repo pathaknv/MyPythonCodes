46d15ae60694f1d298b64b06db9cf40f0c8a57c7
pathaknv <nikhilpathak926@gmail.com>
2016-12-17 00:51:28 +0530

    Basic Version

---
+++ downloadYoutubePlaylist.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup;
+from pytube import YouTube;
+import os;
+import sys;
+import requests;
+
+def downloadVideo(url , path):
+    yt = YouTube(url);
+    mp4files = yt.filter('mp4');
+    print(yt.filename);
+    print(mp4files[-1]);
+    video = yt.get(mp4files[-1].extension , mp4files[-1].resolution);
+    video.download(path);
+
+url = "https://www.youtube.com/playlist?list=PLAwxTw4SYaPlBwWBVoIWKfSWxzuhfSW-D";
+source_code = requests.get(url);
+plain_text = source_code.text;
+soup = BeautifulSoup(plain_text , 'html.parser');
+save_path =  'N:\Videos\Demo\Demo';
+#title = soup.find('h1' ,attrs={'class':'pl-header-title'}).text;
+#playlist_name = title.strip();
+#save_path = os.path.normpath(save_path + '/'+ playlist_name);
+#print(save_path);
+os.mkdir(save_path);
+
+for link in soup.find_all('a' , attrs={'class': 'pl-video-title-link'}) :
+   downloadVideo('https://www.youtube.com/' + link.get('href') , save_path);
